adec8fd7249fa79ffb7f9e102bcc9b93706911e0
basique <basique@fcd8e9d1-ce82-4482-883f-53e604aecffb>
2008-08-20 09:33:48 +0000

    - new Lua plugin: font.lua implements a nicer interface to TTF fonts by caching the glyphs

---
+++ trunk/scripts/font.lua
@@ -0,0 +1,66 @@
+-- [[
+-- $Id$
+-- Font class to ease usage of TTF fonts
+-- See Copyright Notice in COPYRIGHT
+-- ]]
+
+--require"class"
+
+Font = class(function(a, font, size)
+	local str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?()[]{}.,;:-_"
+			
+	local x -- x-coordinate of the next inserted char
+	local char
+	local chars = {}
+	local width, height
+	
+	font = game.addFont(font, size)
+	local defaultImage = font:generateImage(str) 
+	
+	a.font = font
+	a.lineSkip = font:getLineSkip()
+	
+	for i=1,str:len() do
+		char = str:sub(i,i)
+		width, height = font:getTextSize(char)
+		x = font:getTextSize(str:sub(1,i-1))		
+		chars[char] = {
+			0, 0, -- placeholders for the x- and y-coordinates used for rendering
+			image = defaultImage,
+			rect = {x, 0, width, height},
+			color = {255, 255, 255},
+			width = width
+		}
+	end
+	
+	a.chars = chars
+end)
+
+function Font:print(x, y, color, text)
+	local chars = self.chars
+	local char, charTable
+	local orig_x = x
+	local font = self.font
+	
+	for i=1,text:len() do
+		char = text:sub(i,i)
+		if char == "\n" then
+			x = orig_x
+			y = y + self.lineSkip			
+		else			
+			if not chars[char] then
+				chars[char] = {
+					0, 0, -- placeholders for the x- and y-coordinates used for rendering
+					image = font:generateImage(char),
+					width = font:getTextSize(char)
+				}
+			end
+			charTable = chars[char]
+			charTable[1] = x
+			charTable[2] = y
+			charTable.color = color
+			charTable.image:render(charTable)
+			x = x + charTable.width
+		end
+	end
+end
